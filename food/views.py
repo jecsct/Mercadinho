e7b481c705ac0b5af07f3e84f103ee9c68fee8df
Sara Vieira <spgva@iscte-iul.pt>
2022-04-12 10:37:57 +0100

    Teste

--- food/views.py
+++ food/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Viva DIAM. Esta e a pagina de entrada da app votacao.")
+  return HttpResponse("Viva DIAM! Esta e a pagina de entrada da app votacao.")
