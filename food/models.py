ebe11aabe14ab8f8b157590c76b3cde7579edcb0
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 16:21:22 +0100

    Nova Migraçao

--- food/models.py
+++ food/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class Mensagem(models.Model):
-    email_resposta=models.CharField(max_length=50)
-    texto_mensagem=models.CharField(max_length=500)
+    email_resposta = models.CharField(max_length=50)
+    texto_mensagem = models.CharField(max_length=500)
+    dataHora = models.DateTimeField('Data Mensagem Enviada')
--- food/views.py
+++ food/views.py
@@ -6,7 +6,13 @@ def index(request):
   return HttpResponse("Viva DIAM! Esta e a pagina de entrada da app votacao.")
 
 def contactos(request):
-  #if request.method == 'POST':
-
-  #else:
-  return render(request, 'food/contactos.html')
+  if request.method == 'POST':
+    try:
+      email_resposta = request.POST.get("email")
+      texto_mensagem = request.POST.get("texto")
+    except KeyError:
+      return render(request,'food/contactos.html')
+    if email_resposta and texto_mensagem:
+      mensagem = Mensagem(email_resposta=email_resposta, texto_mensagem=texto_mensagem)
+  else:
+    return render(request, 'food/contactos.html')
