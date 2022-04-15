2183f41f996883f99d267787c60fe383b2321f1f
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 16:36:50 +0100

    Reset Migracoes

--- food/views.py
+++ food/views.py
@@ -1,5 +1,9 @@
+from time import timezone
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from food.models import Mensagem
 
 
 def index(request):
@@ -11,8 +15,12 @@ def contactos(request):
       email_resposta = request.POST.get("email")
       texto_mensagem = request.POST.get("texto")
     except KeyError:
-      return render(request,'food/contactos.html')
+      return render(request, 'food/contactos.html')
     if email_resposta and texto_mensagem:
-      mensagem = Mensagem(email_resposta=email_resposta, texto_mensagem=texto_mensagem)
+      mensagem = Mensagem(email_resposta=email_resposta, texto_mensagem=texto_mensagem, dataHora=timezone.now())
+      mensagem.save()
+      return  HttpResponseRedirect(reverse('food:contactos'))
+    else:
+      return HttpResponseRedirect(reverse('food:contactos'))
   else:
     return render(request, 'food/contactos.html')
