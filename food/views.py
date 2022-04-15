b7a7a640c9a07c6aca89600d633e043737357029
jecsct <jetravassos2001@gmail.com>
2022-04-15 19:39:54 +0100

    View detalhe e template detalhe

--- food/views.py
+++ food/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product, Salesman, Comment
 
 
 def index(request):
     return HttpResponse("Viva DIAM. Esta e a pagina de entrada da app votacao.")
 
+def productDetailPage(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'food')
 
 def mapPage(request):
     return render(request, 'food/mercadinhos_map.html', {})
