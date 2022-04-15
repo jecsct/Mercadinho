16784a57715bb16056782edd9b7a9728c867c545
bphbs-iscte <bphbs@iscte-iul.pt>
2022-04-15 16:17:26 +0100

    index,register,login,logout

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
 
+app_name= 'food'
 urlpatterns = [
-  path("", views.index, name="index"),
+  path('', views.index, name='index'),
+  path('registar-utilizador',views.registarutilizador,name='registarutilizador'),
+  path('login-utilizador',views.loginutilizador,name='loginutilizador'),
+  path('logout-utilizador',views.logoututilizador,name='logoututilizador'),
 ]
