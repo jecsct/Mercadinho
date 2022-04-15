a1417e736edef99b891d8a8aae5444874c14de31
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 12:32:55 +0100

    Contactos.html

--- food/urls.py
+++ food/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
+  path("contactos", views.contactos, name="contactos")
 ]
--- food/views.py
+++ food/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Viva DIAM! Esta e a pagina de entrada da app votacao.")
+
+def contactos(request):
+  return render(request, 'food/contactos.html')
