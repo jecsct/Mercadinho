a916a7e5ee0caf58f150694558d76e768c777033
jecsct <jetravassos2001@gmail.com>
2022-04-15 21:05:53 +0100

    Detalhe
    ja e possivel aceder a pagina de detalhe dos produtos

--- food/urls.py
+++ food/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("maps/", views.mapPage, name="mapPage"),
-    path("about/", views.aboutPage, name="aboutPage")
+    path("about/", views.aboutPage, name="aboutPage"),
+    path('<int:product_id>', views.productDetailPage, name='productDetailPage'),
 ]
--- food/views.py
+++ food/views.py
@@ -6,12 +6,15 @@ from .models import Product, Salesman, Comment
 def index(request):
     return HttpResponse("Viva DIAM. Esta e a pagina de entrada da app votacao.")
 
-def productDetailPage(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'food')
 
 def mapPage(request):
     return render(request, 'food/mercadinhos_map.html', {})
 
+
 def aboutPage(request):
     return render(request, 'food/about.html', {})
+
+
+def productDetailPage(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'food/detalhe.html', {'product': product})
