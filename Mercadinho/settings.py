ab5bba12b9369f267ea827a61bb8cc5c448a4cbd
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 13:28:29 +0100

    Contactos template finally working (tive que dar update aos settings foi estranho)

--- Mercadinho/settings.py
+++ Mercadinho/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Mercadinho.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['food/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from . import views
 
+app_name = 'food'
 urlpatterns = [
   path("", views.index, name="index"),
-  path("contactos", views.contactos, name="contactos")
+  path("contactos/", views.contactos, name="contactos")
 ]
