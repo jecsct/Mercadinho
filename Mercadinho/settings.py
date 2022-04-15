8b97b8226e7e6da901e0df4a40aebee28fd7cae3
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 15:13:29 +0100

    Templates Solved

--- Mercadinho/settings.py
+++ Mercadinho/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'food',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Mercadinho.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['food/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
