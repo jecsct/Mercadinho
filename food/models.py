bdea7ba76e2f5c006a48eea41ce0ae16e3c75e3c
Sara Vieira <spgva@iscte-iul.pt>
2022-04-15 15:57:14 +0100

    Criar Mensagem

--- food/models.py
+++ food/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Mensagem(models.Model):
+    email_resposta=models.CharField(max_length=50)
+    texto_mensagem=models.CharField(max_length=500)
--- food/views.py
+++ food/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
   return HttpResponse("Viva DIAM! Esta e a pagina de entrada da app votacao.")
 
 def contactos(request):
+  #if request.method == 'POST':
+
+  #else:
   return render(request, 'food/contactos.html')
