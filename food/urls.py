e3e29832727f0729a783eee3fe1f0385edacd157
bphbs-iscte <bphbs@iscte-iul.pt>
2022-04-16 09:45:39 +0100

    minor errors

--- food/urls.py
+++ food/urls.py
@@ -6,12 +6,11 @@ from .views import redirect_view
 app_name = 'food'
 urlpatterns = [
     path('', redirect_view),
-    path('food/', views.index, name='index'),
-    path('registar-utilizador/', views.registarutilizador, name='registarutilizador'),
-    path('login-utilizador/', views.loginutilizador, name='loginutilizador'),
-    path('logout-utilizador/', views.logoututilizador, name='logoututilizador'),
-    # path("", views.index, name="index"),
-    path('maps/', views.mapPage, name='mapPage'),
-    path('about/', views.aboutPage, name='aboutPage'),
+    path('food', views.index, name='index'),
+    path('registar-utilizador', views.registarutilizador, name='registarutilizador'),
+    path('login-utilizador', views.loginutilizador, name='loginutilizador'),
+    path('logout-utilizador', views.logoututilizador, name='logoututilizador'),
+    path('maps', views.mapPage, name='mapPage'),
+    path('about', views.aboutPage, name='aboutPage'),
     path('<int:product_id>', views.productDetailPage, name='productDetailPage'),
 ]
