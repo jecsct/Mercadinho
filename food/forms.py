033ce0c5babb1ddd44dadb0cfda169df4b64bf88
bphbs-iscte <bphbs@iscte-iul.pt>
2022-05-06 13:19:21 +0100

    resolucao de problemas

--- food/forms.py
+++ food/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm, EmailInput, DateInput, PasswordInput
+from django.forms import ModelForm, EmailInput, DateInput
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 import datetime
 
-from food.models import Customer, Mensagem, Salesman
+from food.models import Customer, Mensagem, Salesman, Product
 from django import forms
 
 
@@ -32,6 +32,14 @@ class CustomerForm(ModelForm):
         }
 
 
+class SalesmanForm(ModelForm):
+    rating = forms.IntegerField(initial=0)
+
+    class Meta:
+        model = Salesman
+        fields = ('rating', 'profile_pic', 'phone_number')
+
+
 class ContactForm(ModelForm):
     dataHora = forms.DateTimeField(initial=datetime.datetime.now())
     texto_mensagem = forms.Textarea()
@@ -41,9 +49,11 @@ class ContactForm(ModelForm):
         fields = ('email', 'texto_mensagem', 'dataHora')
 
 
-class SalesmanForm(ModelForm):
-    rating = forms.IntegerField(initial=0)
+class AddProductForm(ModelForm):
+    sales = forms.IntegerField(initial=0)
+    views = forms.IntegerField(initial=0)
+    rating = forms.DecimalField(initial=0)
 
     class Meta:
-        model = Salesman
-        fields = ('rating', 'profile_pic', 'phone_number')
+        model = Product
+        fields = ('name', 'description', 'price', 'image', 'sales', 'views', 'rating')
